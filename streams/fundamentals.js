902e2a00412d29a981cd5dcee0fa338dcde9d54f
Tallysson Yuri <tallyssonyuri03@gmail.com>
2025-01-21 21:19:33 -0300

    conhecendo o conceito de streams com o node.js

---
+++ streams/fundamentals.js
@@ -0,0 +1,42 @@
+// Stream é a possibilidade de trabalhar com partes do dado enquanto ele é processado
+
+// Netflix e Spotify são streams de filmes e músicas
+
+import { Readable, Writable, Transform } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+    index = 1;
+    
+    _read() {
+        const i = this.index ++;
+
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+    
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+class InverseNumber  extends Transform {
+    _transform(chunk, encoding, callback){
+        const tranformed = Number(chunk.toString() * -1)
+
+        callback(null, Buffer.from((String(tranformed))))
+    }
+}
+
+new OneToHundredStream()
+    .pipe(new InverseNumber())
+    .pipe(new MultiplyByTenStream())
